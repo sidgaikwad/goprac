dea8283dbd0c46f9f6f6b57d15bc27e43ef2a6a8
sidgaikwad <gaikwadsiddharth03@gmail.com>
2024-04-14 01:03:09 +0530

    trying to see errors

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,9 +1,10 @@
--- +goose up
+-- +goose Up
 CREATE TABLE users (
     id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     name TEXT NOT NULL
 );
--- +goose down
+
+-- +goose Down
 DROP TABLE users;
